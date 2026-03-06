dfbaf73185413190fc9d2540471ce9485523a93e
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-06 15:56:46 +0100

    add main compontens structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+const temponaryText = '--> Content goes here <--'
+
+export default function Main() {
+
+
+    return(
+        <main>
+            <span>{temponaryText}</span>
+        </main>
+    )
+}
